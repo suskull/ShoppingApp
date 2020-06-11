767101ecb032420069b11524c8f84f89e4dc492c
suskull <xoaixanh.muoii95@gmail.com>
2020-06-11 09:10:42 +0700

    using redux-persist for local store

--- src/index.js
+++ src/index.js
@@ -3,16 +3,22 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import {Provider} from 'react-redux';
 
+//localstore
+import {PersistGate} from 'redux-persist/integration/react';
+import {store ,persistor} from './redux/store';
+
 
 import './index.css';
 import App from './App';
 
-import store from './redux/store';
 
 ReactDOM.render(
   <Provider store ={store}>
     <BrowserRouter>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
+      
     </BrowserRouter>
   </Provider>,
   document.getElementById('root')
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,11 +1,22 @@
 import { combineReducers } from 'redux';
+//localstore
+import {persistReducer} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user/user.reducer';
 import cartReducer  from './cart/cart-reducer';
 
-export default combineReducers({
+//localstore
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart']
+}
+//localstore rootReducer
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer
- 
+})
 
-});
+//localstore
+export default persistReducer(persistConfig,rootReducer);
