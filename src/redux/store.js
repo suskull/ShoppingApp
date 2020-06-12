3b717853155cf1bd7dd1c5ec84ef2d603d54763c
suskull <xoaixanh.muoii95@gmail.com>
2020-06-12 16:47:50 +0700

    optimizing project build

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,8 +5,15 @@ import logger from 'redux-logger';
 import {persistStore} from 'redux-persist';
 
 import rootReducer from './root-reducer';
+// 
+//const middlewares = [logger];
 
-const middlewares = [logger];
+const middlewares = []
+
+//optimizing product build, if there no changes in node_modules , there will no update, so building is faster
+if(process.env.NODE_ENV==='development') {
+    middlewares.push(logger)
+}
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
