766c813361e7aa9815219b592b98ad0721253a8e
suskull <xoaixanh.muoii95@gmail.com>
2020-06-26 02:06:51 +0700

    a

--- src/pages/contact/contact.jsx
+++ src/pages/contact/contact.jsx
@@ -4,10 +4,7 @@ import './contact.scss';
 function Contact() {
     return(
         <div className = "contact">
-            <h2>Contact me</h2>
-            <span>Leave me a message at</span> 
-            <p>xoaixanh.muoii95@gmail.com</p>
-            <p>0963641003</p>
+            
         </div>
 
     )
